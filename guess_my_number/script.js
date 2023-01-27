6d5959aa9f7ff92f412257b824476a43d8c66bfa
Mertcan Yılmaz <107379154+smertcanyilmaz@users.noreply.github.com>
2023-01-28 00:13:42 +0300

    Update script.js

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -35,3 +35,13 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
