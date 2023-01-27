75d1d9a04549decde3177465102d2d5e7c95aa78
Mertcan Yılmaz <107379154+smertcanyilmaz@users.noreply.github.com>
2023-01-28 00:29:33 +0300

    Update script.js

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -3,9 +3,11 @@
 // document.querySelector('.message').textContent = 'Correct Number!';
 // document.querySelector('.guess').value = 23;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+
+const displayMessage = function (message) {
+document.querySelector('.message').textContent = message;};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
